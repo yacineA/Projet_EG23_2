bdfb18deec8d7846f897e6b11911727947d99548
ANZAR Yassine <anzaryassine@gmail.com>
2017-05-25 15:44:06 +0200

    Initial commit

---
+++ app/src/test/java/fr/utt/eg23/projet/projet_eg23/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package fr.utt.eg23.projet.projet_eg23;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
